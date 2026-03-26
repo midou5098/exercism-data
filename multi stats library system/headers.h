c2815e32ebf4ca8ac84e852c4833bc07008b3237
mohamed amdouni <midouamdouni4@gmail.com>
2026-03-26 10:37:23 +0000

    started the c++ learning path

---
+++ multi stats library system/headers.h
@@ -0,0 +1,21 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include <vector>
+#include <iostream>
+class library{
+    book:
+        std::string name;
+        std::string author;
+        std::string genre;
+        std::int pages;
+        std::bool borrowed;
+        std::double 
+
+
+
+}
+
+
+
+#endif
